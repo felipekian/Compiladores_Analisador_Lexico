853b5cd5fec1febfdd6656310ac049c00a7bb15c
felipe <felipekian@yahoo.com.br>
2020-03-18 20:09:18 -1200

    iniciando o desenvolvimento da linguagem e das palavras reservadas

---
+++ grammar.py
@@ -0,0 +1,16 @@
+
+#palavras reservadas
+reserved_words = ['while', 'for', 'do', 'return', 'break', 'continue', 'switch', 'case']
+
+#comentarios
+comments = ['//', '#']
+
+#final de instruções
+end_of_instructions = [';']
+
+#operadores
+operators = ['+', '-', '/', '*', '^']
+
+#atribuidores
+assigners = ['=', '+=', '-=', '/=', '*=']
+
